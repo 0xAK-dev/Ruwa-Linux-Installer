8d8ebd772a056ab4dba84c4ca6b1c47a229ea1ab
0xAK-dev <akgithub009@gmail.com>
2026-08-10 19:57:20 +0200

    Set logging level to ERROR

--- core/installer.py
+++ core/installer.py
@@ -5,7 +5,7 @@ from pathlib import Path, PurePath
 
 from config import VERSION
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
+logging.basicConfig(level=logging.ERROR, format="%(asctime)s [%(levelname)s] %(message)s")
 logger = logging.getLogger("RuwaInstaller")
 
 
